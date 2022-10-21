605af5dd90842b0925cbb0106ae8204b604b64ec
Monirul-RS <monir030599@gmail.com>
2022-10-21 20:47:23 +0600

    adsdasda

---
+++ src/__auth.steps.js
@@ -0,0 +1,33 @@
+/*
+AUTH SETUP
+1.Create firebase project
+2.enable web
+3.enable sign in method
+4. install firebase(npm i firebase)
+5. install react router dom
+6. get firebase config in firebase.config.js
+7.export app from firebase.config.js
+
+*/
+
+/*
+1.Create user context(Auth Context): UserContext --> Component name,
+userContext Provides AuthContext
+2.Create AuthContext
+3.Set AuthContext.Provider 
+4.destructure children {children}
+5.
+    <AuthContext.Provider>
+        {children}
+    <AuthContext.Provider>
+6. const authInfo = {}
+and <AuthContextProvider value={authInfo}>
+7.go to index.js and
+    <UserContext>
+        <App/>
+    </UserContext
+
+
+
+
+*/ 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMrDjvXFmJ7RRKKAVoCFacWS2GZFklhmk",
+  authDomain: "ema-john-simple-758b4.firebaseapp.com",
+  projectId: "ema-john-simple-758b4",
+  storageBucket: "ema-john-simple-758b4.appspot.com",
+  messagingSenderId: "382940737973",
+  appId: "1:382940737973:web:6663b4ca84b9813f9ee9fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
